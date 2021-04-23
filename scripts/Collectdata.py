7fc7206c8f540d3e13e2d2d9e70142ebb42f26c5
JakobH <71091384+Jakobk4@users.noreply.github.com>
2021-04-23 11:35:48 +0200

    Create Collectdata.py

---
+++ scripts/Collectdata.py
@@ -0,0 +1,17 @@
+from FormatData import FormatData
+def FindData(search = str):
+    """Returns data from specified country"""
+    data = FormatData()
+    countries = data["Country_Region"]
+    index = []
+    result = {}
+    for i in countries:
+        if countries[i] == search:
+            index.append(i)
+    for title in list(data):
+        for i in index:
+            if title not in result:
+                result[title] = (str(data[title][i]) + " ")
+            else:
+                result[title] += (str(data[title][i]) + " ")
+    return result
