2cc324ac24aaff39c67c70a29b61fc237d87e574
Fabian Zander <53708288+Fabbster@users.noreply.github.com>
2021-04-26 11:10:56 +0200

    Delete CovGraphs.py

--- scripts/CovGraphs.py
+++
@@ -1,9 +0,0 @@
-import plotly.express as px
-import FormatData
-
-formatedData = FormatData.FormatData()
-for item in formatedData.items():
-    print(item)
-
-fig = px.bar(formatedData, x='Country_Region', y='Confirmed')
-fig.write_html('Confirmed.html', auto_open=True)
